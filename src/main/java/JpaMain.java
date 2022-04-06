328c8f139c1938fe278487111d71ec3869a22831
qkrtpgh5033 <qkrtpgh5033@gmail.com>
2022-04-06 20:48:20 +0900

    chapter-프록시와 연관관계 관리

--- src/main/java/JpaMain.java
+++ src/main/java/JpaMain.java
@@ -16,7 +16,7 @@ public class JpaMain {
          * 모든 데이터 변경은 "트랜잭션이 필요"
          */
         EntityTransaction transaction = em.getTransaction();
-//        transaction.begin();
+        transaction.begin();
 
         //code
         try{
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -19,15 +19,18 @@ public class Order extends BaseEntity {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
-    @OneToOne
+    /**
+     * cascade -> Order를 생성해서 Delivery를 넣을 때 자동으로 Order를 저장하면 Delivery도 저장
+     */
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "DELIVERY_ID")
     private Delivery delivery;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime orderDate;
